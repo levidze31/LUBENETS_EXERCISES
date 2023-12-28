f90506ce91bc7dc05fa1a2a50301b1298d901d53
levidze31 <154834028+levidze31@users.noreply.github.com>
2023-12-28 15:44:24 +0300

    Update ZADANIE_1.py
    Added a condition

--- ZADANIE_1.py
+++ ZADANIE_1.py
@@ -1,4 +1,7 @@
-n=int(input('n=?'))
-k=int(input('k=?'))
-for i in range (0,n):
-    print(k)
+K = int (input('Введите значение K:'))
+N = int (input('Введите значение N:'))
+if K>0 and N>0:
+    for i in range(0,N):
+        print(K)
+else:
+    print('K<0 и N<0')
